3bd25c297e47baff28edd744643c5fcd879dafb6
Riska <riska607@gmail.com>
2026-09-25 20:00:41 +0700

    Tugas Praktikum Modul 1

---
+++ Modul-1/24-081_Riska Nana Nuril Fadilah/soal 3.php
@@ -0,0 +1,19 @@
+<?php 
+//ini non embedded script
+echo "Hello world";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>modul 1</title>
+</head>
+<body> 
+	<?php
+	// ini embedded script 
+	echo "Hello world";
+	?>
+</body>
+</html>
